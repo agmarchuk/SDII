0e1bf47e9243a0b05510cc65483a2dc77109b0df
Sergey303 <s@s.s>
2016-05-03 16:01:09 +0600

    test form

--- P04_SimpleTripleStore_Phototeka/Program.cs
+++ P04_SimpleTripleStore_Phototeka/Program.cs
@@ -4,79 +4,96 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using SDII;
 
 namespace P04_SimpleTripleStore_Phototeka
 {
     public static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            string path = "../../../Databases/simple triple store";
-            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            string path = "../../";
+            if (!Directory.Exists(path+ "../Databases/simple triple store")) Directory.CreateDirectory(path+ "../Databases/simple triple store");
             Console.WriteLine("Start TestGenerator");
+            TextWriter res = new StreamWriter(new FileStream(path + "res.txt", FileMode.Append, FileAccess.Write));
+            XElement xcnf = XElement.Load(path + "tests.xml");
+            XElement xcommon = XElement.Load(path + "../common.xml");
+            xcommon.Add(xcnf);
+            Random rnd;
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
 
-            SimpleTripleStore.SimpleTripleStore simpleTripleStore=new SimpleTripleStore.SimpleTripleStore(path, 1000*1000);
-        
-            int npersons = 400000; // 40 тыс. персон соответствует 1 млн. триплетов 
-            bool toload = true;
-            if (toload)
+            SimpleTripleStore.SimpleTripleStore simpleTripleStore =
+                new SimpleTripleStore.SimpleTripleStore(path + "../Databases/simple triple store",
+                    1000*1000);
+            foreach (XElement xprobe in xcnf.Elements())
             {
-                Directory.Delete(path);
-                sw.Restart();
-                Polar.Data.Phototeka generator = new Polar.Data.Phototeka(npersons, 777777);
-                simpleTripleStore.Build(generator.GenerateRDF());
-                sw.Stop();
-                Console.WriteLine("Load ok. Duration={0}", sw.ElapsedMilliseconds); // 10000: 14.9 сек.
+                ProbeFrame probe = new ProbeFrame(xprobe.AncestorsAndSelf().Attributes());
+                int npersons = (int) probe.siz;
+                if (probe.sol == "simpleTripleStore_load")
+                {
+                    Directory.Delete(path + "../Databases/simple triple store", true);
+                    sw.Restart();
+                    Polar.Data.Phototeka generator = new Polar.Data.Phototeka(npersons, 777777);
+                    simpleTripleStore.Build(generator.GenerateRDF());
+                    sw.Stop();
+                    Console.WriteLine("Load ok. Duration={0}", sw.ElapsedMilliseconds); // 10000: 14.9 сек.
+                    probe.ndx = sw.ElapsedMilliseconds;
+                    res.WriteLine(probe.ToCSV());
+                }
+                else if (probe.sol == "simpleTripleStore_SelectById")
+                {
+                    rnd = new Random(777777777);
+                    sw.Restart();
+                    long sum = 0;
+                    for (int i = 0; i < probe.nte; i++)
+                    {
+                        int id = rnd.Next(0, (int) probe.siz - 1);
+                        sum += simpleTripleStore.GetDirects(id).Count();
+                    }
+                    sw.Stop();
+                    probe.tim = sw.ElapsedMilliseconds;
+                    probe.sum = sum;
+                    Console.WriteLine("SelectById ok. Duration={0}", sw.ElapsedMilliseconds); // 7
+                    res.WriteLine(probe.ToCSV());
+                }
+                else if (probe.sol == "simpleTripleStore_SearchByName")
+                {
+                    rnd = new Random(777777777);
+                    sw.Restart();
+                    long sum = 0;
+                    for (int i = 0; i < probe.nte; i++)
+                    {
+                        int id = rnd.Next(0, (int) probe.siz - 1);
+                        sum += (int) simpleTripleStore.GetSubjects("name", "Pupkin" + id/10).Count();
+                    }
+                    sw.Stop();
+                    probe.tim = sw.ElapsedMilliseconds;
+                    probe.sum = sum;
+                    Console.WriteLine("SearchByName ok. Duration={0}", sw.ElapsedMilliseconds); // 7
+                    res.WriteLine(probe.ToCSV());
+                }
+                else if (probe.sol == "simpleTripleStore_GetRelationByPerson")
+                {
+                    rnd = new Random(777777777);
+                    sw.Restart();
+                    long sum = 0;
+                    for (int i = 0; i < probe.nte; i++)
+                    {
+                        int id = rnd.Next(0, (int) probe.siz - 1);
+                        sum += simpleTripleStore.GetSubjects("reflected", id.ToString()).Count();
+                    }
+                    sw.Stop();
+                    probe.tim = sw.ElapsedMilliseconds;
+                    probe.sum = sum;
+                    Console.WriteLine("GetRelationByPerson ok. Duration={0}", sw.ElapsedMilliseconds); // 7
+                    res.WriteLine(probe.ToCSV());
+                }
+                else if (probe.sol == "unused")
+                {
+                }
             }
-
-            sw.Restart();
-            simpleTripleStore.GetSubjects("a","person").Count();
-            sw.Stop();
-            Console.WriteLine("Count ok. Duration={0}", sw.ElapsedMilliseconds); // 1200
-
-            sw.Restart();
-            simpleTripleStore.GetDirects(2870).Count();
-            //db.SelectById(1309, "person");
-            sw.Stop();
-            Console.WriteLine("SelectById ok. Duration={0}", sw.ElapsedMilliseconds); // 7
-
-            sw.Restart();
-            simpleTripleStore.GetSubjects("name", "Pupkin999").Count();
-            sw.Stop();
-            Console.WriteLine("SearchByName ok. Duration={0}", sw.ElapsedMilliseconds); // 7
-
-            sw.Restart();
-            simpleTripleStore.GetSubjects("reflected", 2870.ToString());
-            //db.GetRelation(1309, "reflected");
-            sw.Stop();
-            Console.WriteLine("GetRelation ok. Duration={0}", sw.ElapsedMilliseconds); // 91
-
-            // Проверка серии портретов
-            Random rnd = new Random(999999); int i = 0;
-            sw.Restart();
-            for (; i < 100; i++) simpleTripleStore.GetSubjects("reflected", rnd.Next(10000).ToString());
-            sw.Stop();
-            System.Console.WriteLine("GetPortraitById OK. times={0} duration={1}", i, sw.ElapsedMilliseconds); // 1227 ms. 
-
-            // Проверка серии портретов (2) по тем же идентификаторам
-            rnd = new Random(999999); i = 0;
-            sw.Restart();
-            for (; i < 100; i++) simpleTripleStore.GetSubjects("reflected", (rnd.Next(10000)).ToString());
-            sw.Stop();
-            System.Console.WriteLine("GetPortraitById OK. times={0} duration={1}", i, sw.ElapsedMilliseconds); // 43 ms. 
-
-            // 
-            Console.WriteLine("==== Динамика естественного разогрева ====");
-            Random rnd2 = new Random();
-            for (int j = 0; j < 100; j++)
-            {
-                sw.Restart();
-                for (int ii = 0; ii < 100; ii++) simpleTripleStore.GetSubjects("reflected", rnd2.Next(npersons - 1).ToString());
-                sw.Stop();
-                Console.Write("{0} ", sw.ElapsedMilliseconds);
-            }
-            System.Console.WriteLine(); // 
+            res.Close();
         }
     }
 }
